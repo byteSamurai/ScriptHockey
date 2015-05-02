96941c83cbc92b630d244bff5e85bafb3178b0d2
SimoKutlin <simo.kutlin@gmail.com>
2015-05-02 17:37:13 +0200

    Adding first field implementation

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', function (req, res, next) {
     res.render('techdemo', { title: 'Just a techdemo' });
 });
 
+/* Field dev. */
+router.get('/field', function (req, res, next) {
+    res.render('field', { title: 'Field under construction' });
+});
+
 module.exports = router;
 
 
